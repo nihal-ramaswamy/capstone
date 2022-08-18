476e7903efa77d5245185d4125f40ed5999dc4e6
Nihal Ramaswamy <41371273+nihal-ramaswamy@users.noreply.github.com>
2022-08-18 17:57:55 +0530

    Updated UserMedia

--- components/UserMedia/UserMedia.component.tsx
+++ components/UserMedia/UserMedia.component.tsx
@@ -1,18 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import { selectMediaState, setMediaState } from "../../store/mediaSlice";
+import { useState } from "react";
+import Audio from "../Audio/Audio.component";
+import Video from "../Video/Video.component";
 
 const UserMedia = () => {
     const mediaState = useSelector(selectMediaState);
     const dispatch = useDispatch();
+    const [image, setImage] = useState(null);
+    const [audio, setAudio] = useState(null);
+
+    // dispatch(setMediaState(!mediaState))
 
     return (
         <div>
-            <div>
-                {mediaState ? "AAAAA" : "BBBBB"}
-            </div>
-            <button onClick={() => dispatch(setMediaState(!mediaState))}>
-                Click Here
-            </button>
+            <button onClick={() => dispatch(setMediaState(!mediaState))}>Record</button>
+            <Audio />
+            <Video />
         </div>
     );
 }
