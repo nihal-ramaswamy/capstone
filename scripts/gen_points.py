6dcbca8ac84a92c49e218a6ef49a44915f5c21cb
Ghosh-Anisha <73275208+Ghosh-Anisha@users.noreply.github.com>
2022-11-10 11:08:43 +0530

    Preprocessing not yet completed

--- scripts/gen_points.py
+++ scripts/gen_points.py
@@ -37,18 +37,17 @@ def generate_data(img, audio,time):
     roll = img_result[0]
     pitch = img_result[1]
     yaw = img_result[2]
-    noOfFaces = img_result[3]
+    no_of_faces = img_result[3]
     face_recognition_result = face_recognition1.face_detect(img)
     data = []
 
-    data.append(img)
     data.append(time)
     data.append(volume)
     data.append(speech)
     data.append(roll)
     data.append(pitch)
     data.append(yaw)
-    data.append(noOfFaces)
+    data.append(no_of_faces)
     data.append(face_recognition_result[0])    
     #run the model here and append the result to data
     #If result is true, save the img and audio to the database
@@ -56,19 +55,52 @@ def generate_data(img, audio,time):
 
 # answer = generate_data('anisha2.jpg', 'male.wav', 0)
 # print(answer)
+
+def preprocess(data):
+    #preprocess the data
+    time = data[0]
+    volume = data[1]
+
+    speech = data[2]
+    if data[3] < 0: roll = data[3]*-1 
+    else: roll = data[3]
+
+    if data[4] < 0: pitch = data[4]*-1
+    else: pitch = data[4]
+    pitch = 180 - pitch 
+
+    if data[5] < 0: yaw = data[5]*-1
+    else: yaw = data[5]
+
+    no_of_faces = data[6]
+    face_recognition_result = data[7]
+
+    volume.insert(0,no_of_faces)
+    volume.insert(0,yaw)
+    volume.insert(0,pitch)
+    volume.insert(0,roll)
+    volume.insert(0,speech)
+    volume.insert(0,face_recognition_result)
+
+    data = np.array(volume)
+    
+
+    return data
+
 def selectValuesFromUsersWhereUserID(userID: str) -> json:
     val = (storage.child("users").child(userID).get()).val()
     print(val)
     img = val['snapshot']
     audio = val['voice']
     time = val['timestamp']
-    generate_data(img,audio,time)
-    return val
+    modelData = generate_data(img,audio,time)
+    return modelData
 
 
 def validate(dbValue) -> int:
     model = pickle.load(model)
-
+    score = model.predict(dbValue)
+    return score
 
 if __name__ == "__main__":
     print(selectValuesFromUsersWhereUserID(userID))
