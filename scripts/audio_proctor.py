9bdf6601f4cfc594b551fbd2c7fc4db20fca8f84
Ghosh-Anisha <73275208+Ghosh-Anisha@users.noreply.github.com>
2022-11-14 19:38:14 +0530

    Speech analysis done

--- scripts/audio_proctor.py
+++ scripts/audio_proctor.py
@@ -1,38 +1,40 @@
 import numpy as np
-import webrtcvad
 import base64
+import torch
 import librosa 
+
+
+torch.set_num_threads(1)
+
 
 def audio_analysis(audioString):
 
     wavFile = open("audioFile.wav", "wb")
-    decodeString = base64.b64decode(audioString)
+    decodeString = base64.b64decode(audioString[20:])
     wavFile.write(decodeString)
     wavFile.close()
-
     #MFCC Features 
-    audioWave, sampleRate = librosa.load('audioFile.wav')
+    audioWave, sampleRate = librosa.load("audioFile.wav")
     features = librosa.feature.mfcc(y=audioWave, sr=sampleRate, n_mfcc=40)
     features = np.mean(features.T,axis=0) 
 
-    #Speech Detection
-    counter=0
-    totalCount=0
-    audioFrames = audioWave.readframes(800)#arg here divides the audio file into atmost arg number of frames
-    vad = webrtcvad.Vad() # Now Run the VAD on 10 ms of silence and 16000 sampling rate 
-    frameDuration = 10  # in ms
-    for audioFrame in audioFrames : # Detecting speech
+    # Speech Detection
+    model, utils = torch.hub.load(repo_or_dir='snakers4/silero-vad',
+                              model='silero_vad',
+                              force_reload=True)
 
-        finalFrame = audioFrame.to_bytes(2,"big")* int(sampleRate * frameDuration / 1000) #multiplying by sample rate and frame duration because the vad requires a particular data format
-        totalCount=totalCount+1
+    (get_speech_timestamps,
+    save_audio,
+    read_audio,
+    VADIterator,
+    collect_chunks) = utils
 
-        if(vad.is_speech(finalFrame, sampleRate)):
-            counter=counter+1
-          
-    if len(volume) > 6400:
-        volume = volume[:6400]
-        
-    if((counter/totalCount)>=0.5):
+
+    # full audio
+    # get speech timestamps from full audio file
+    speech_timestamps = get_speech_timestamps(audioWave, model, sampling_rate=16000)
+    if len(speech_timestamps)>=4:
         return features, True
-    return features , False
+    else:
+        return features, False
 
