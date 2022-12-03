2d9481a4780bcd3b201a44c1b04f1646d0b49c1a
Nihal Ramaswamy <nihalramaswamy@gmail.com>
2022-12-03 21:53:51 +0530

    Removed unnecessary folders

--- handlers/recordingsList.handlers.ts
+++
@@ -1,5 +0,0 @@
-import { SetRecordings } from "../types/Audio.types";
-
-export const deleteAudio = (audioKey: string, setRecordings: SetRecordings) => {
-    setRecordings((prevState) => prevState.filter((record) => record.key !== audioKey));
-}
